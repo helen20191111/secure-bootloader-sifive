99cb32cc99bcab7dc796e5fc301f77bd40012d08
Yann Gaude <yann.gaude@sifive.com>
2019-12-09 15:18:20 +0100

    Update licence text

--- SecureBootROM/include/sbrm/sbrm_internal.h
+++ SecureBootROM/include/sbrm/sbrm_internal.h
@@ -1,4 +1,3 @@
-/** pi_public.h */
 /**
  * Copyright 2019 SiFive
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
@@ -10,17 +9,38 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
 */
-
-#ifndef _PI_PUBLIC_H_
-#define _PI_PUBLIC_H_
+#ifndef SBRM_SBRM_INTERNAL_H_
+#define SBRM_SBRM_INTERNAL_H_
 
 /** Global includes */
+#include <memory.h>
+#include <common.h>
 #include <errors.h>
-#include <patch.h>
+#include <metal/cpu.h>
 /** Other includes */
 /** Local includes */
 
-#endif /* _PI_PUBLIC_H_ */
+/** External declarations */
+/** Local declarations */
+
+/** Defines *******************************************************************/
+
+/** Enumerations **************************************************************/
+
+
+/** Structures ****************************************************************/
+
+
+/** Functions *****************************************************************/
+void sbrm_set_power_mode(uint32_t power_mode);
+int32_t sbrm_selftest(t_context *p_ctx);
+int32_t sbrm_compute_crc(uint32_t *p_crc, uint8_t *p_data, uint32_t size);
+
+/** Macros ********************************************************************/
+
+
+
+#endif /* SBRM_SBRM_INTERNAL_H_ */
 
 /******************************************************************************/
 /* End Of File */
